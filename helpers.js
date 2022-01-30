43aa6fa674e4869492ef064fd5a09aede42d138f
mark <mr.karimi1377@gmail.com>
2022-01-31 00:25:46 +0330

    add message animation

--- helpers.js
+++ helpers.js
@@ -1,7 +1,36 @@
-function showAlert(procNum) {
-    $('#alert-' + procNum).removeClass('hide').addClass('show');
-    $("#alert-" + procNum).delay(2000).slideUp(200, function() {
+function showAlert(receiver, type) {
+    $(`#${type}-${receiver}`).removeClass('hide').addClass('show');
+    $(`#${type}-${receiver}`).delay(2000).slideUp(200, function() {
         $(this).removeClass('show').addClass('hide');
+        $(this).removeAttr("style");
+    });
+}
+
+function showLTRMessageAnimation(sender) {
+    $(`.proc-${sender}-ltr`).css('color', 'orange');
+    anime({
+        targets: `.proc-${sender}-ltr`,
+        duration: 1000,
+        left: ['0px', '150px'],
+        easing: 'linear',
+        direction: 'normal',
+    });
+    $(`.proc-${sender}-ltr`).delay(1000).slideUp(200, function() {
+        $(this).removeAttr("style");
+    });
+}
+
+function showRTLMessageAnimation(sender) {
+    $(`.proc-${sender}-rtl`).css('color', 'orange');
+    anime({
+        targets: `.proc-${sender}-rtl`,
+        duration: 1000,
+        left: ['150px', '0px'],
+        easing: 'linear',
+        direction: 'normal',
+        autoPlay: false,
+    });
+    $(`.proc-${sender}-rtl`).delay(1000).slideUp(200, function() {
         $(this).removeAttr("style");
     });
 }
--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-let animation = anime({
-    targets: '.message',
-    // translateX: 200,
-    duration: 1000,
-    left: ['0px', '150px'],
-    // right: ['150px', '0px'],
-    easing: 'linear',
-    direction: 'normal',
-    autoplay: false,
-});
-
-
-function play() {
-    animation.restart();
-};
+// function play() {
+//     animation.restart();
+// };
 
 var clock1 = 0;
 var clock2 = 0;
@@ -27,18 +15,22 @@ var intervalId = setInterval(function() {
     clock3 += 3;
 }, 1000);
 
-// Send message from sender to receiver
 function sendMessage(sender, receiver) {
-    console.log('send');
-    receiveMessage(receiver, { 'timestamp': eval('clock' + sender) });
+    if (sender < receiver) {
+        showLTRMessageAnimation(sender);
+    } else {
+        showRTLMessageAnimation(sender);
+    }
+
+    receiveMessage(receiver, { 'timestamp': eval(`clock${sender}`) });
 }
 
 function receiveMessage(receiver, message) {
-
-    if (message.timestamp >= eval('clock' + receiver)) {
-        showAlert(receiver);
-        window['clock' + receiver] = message.timestamp + 1;
-    }
+    setTimeout(function() {
+        showAlert(receiver, 'receive');
+        if (message.timestamp >= eval(`clock${receiver}`)) {
+            showAlert(receiver, 'sync');
+            window['clock' + receiver] = message.timestamp + 1;
+        }
+    }, 1000);
 }
-
-document.querySelector('.message').onclick = play;
