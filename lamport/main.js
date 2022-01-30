9573fb3b68204cb94c2e13d13856e4c6b5210bd4
mark <mr.karimi1377@gmail.com>
2022-01-31 00:58:04 +0330

    get speed from user

--- lamport/main.js
+++ lamport/main.js
@@ -1,12 +1,29 @@
 var [clock1, clock2, clock3] = [0, 0, 0];
 
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+var p1speed = 1;
+var p2speed = 2;
+var p3speed = 3;
+
+if (params.p1speed != null) {
+    p1speed = params.p1speed * 1;
+}
+if (params.p2speed != null) {
+    p2speed = params.p2speed * 1;
+}
+if (params.p3speed != null) {
+    p3speed = params.p3speed * 1;
+}
+
 var intervalId = setInterval(function() {
     $('#clock1').text(clock1);
     $('#clock2').text(clock2);
     $('#clock3').text(clock3);
-    clock1++;
-    clock2 += 2;
-    clock3 += 3;
+    clock1 += p1speed;
+    clock2 += p2speed;
+    clock3 += p3speed;
 }, 1000);
 
 function sendMessage(sender, receiver) {
